275f1200d208d726d46fdf25c33e23f756a56743
Yahia Elsayed <yahiaelsayed19@gmail.com>
2022-12-02 17:23:32 +0200

    ItemPage list key

--- src/pages/ItemPage.js
+++ src/pages/ItemPage.js
@@ -12,7 +12,7 @@ const ItemPage = () => {
     const { title, year, image, description, stars, ids } = extractData(data, type)
     const genres = extractGenres(ids, type)
     const itemGenere = genres.map(genre =>
-        <li>{genre}</li>)
+        <li key={new Date()}>{genre}</li>)
     return (
         <>
             <div className={classes['item-page']}>
