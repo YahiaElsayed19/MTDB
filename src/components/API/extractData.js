49a2caa465d5a0f3c693816bd5b91816557aef48
Yahia Elsayed <yahiaelsayed19@gmail.com>
2022-12-02 15:32:48 +0200

    extractGenres

--- src/components/API/extractData.js
+++ src/components/API/extractData.js
@@ -8,49 +8,8 @@ const extractData = (result, itemType) => {
     ]?.slice(0, 4);
     const description = result["overview"];
     const stars = result["vote_average"].toFixed(1);
-    const genreId = {
-        movie: {
-            28: "Action",
-            12: "Adventure",
-            16: "Animation",
-            35: "Comedy",
-            80: "Crime",
-            99: "Documentary",
-            18: "Drama",
-            10751: "Family",
-            14: "Fantasy",
-            36: "History",
-            27: "Horror",
-            10402: "Music",
-            9648: "Mystery",
-            10749: "Romance",
-            878: "Science Fiction",
-            10770: "TV Movie",
-            53: "Thriller",
-            10752: "War",
-            37: "Western",
-        },
-        tv: {
-            10759: "Action & Adventure",
-            16: "Animation",
-            35: "Comedy",
-            80: "Crime",
-            99: "Documentary",
-            18: "Drama",
-            10751: "Family",
-            10762: "Kids",
-            9648: "Mystery",
-            10763: "News",
-            10764: "Reality",
-            10765: "Sci-Fi & Fantasy",
-            10766: "Soap",
-            10767: "Talk",
-            10768: "War & Politics",
-            37: "Western",
-        },
-    };
-
-    return { title, image, year, description, stars };
+    const ids = result['genre_ids']
+    return { title, image, year, description, stars, ids };
 };
 
 export default extractData;
--- src/pages/ItemPage.js
+++ src/pages/ItemPage.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { useLocation } from 'react-router-dom'
 import extractData from '../components/API/extractData'
+import extractGenres from '../components/API/extractGenres'
 
 const ItemPage = () => {
     const location = useLocation()
     const data = location.state.itemData;
     const type = location.state.itemType;
-    const { title, year, image, description ,stars} = extractData(data, type)
+    const { title, year, image, description, stars, ids } = extractData(data, type)
+    const genres = extractGenres(ids, type)
     return (
         <>
             <div>
@@ -16,13 +18,16 @@ const ItemPage = () => {
                 {year}
             </div>
             <div>
-                <img src={image} alt={title}/>
+                <img src={image} alt={title} />
             </div>
             <div>
                 {description}
             </div>
             <div>
                 {stars}
+            </div>
+            <div>
+                {genres}
             </div>
         </>
     )
